281abcb8b9dc3e8f4b41ebdf10a491037205ebed
villanub <villanub@gmail.com>
2015-06-19 11:35:58 -0500

    Initial website

---
+++ AzureDeployDev/Controllers/UserProfileController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using Microsoft.Azure.ActiveDirectory.GraphClient;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using AzureDeployDev.Models;
+
+namespace AzureDeployDev.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private string appKey = ConfigurationManager.AppSettings["ida:ClientSecret"];
+        private string graphResourceID = "https://graph.windows.net";
+
+        // GET: UserProfile
+        public ActionResult Index()
+        {
+            string tenantID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+            string userObjectID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+            try
+            {
+                Uri servicePointUri = new Uri(graphResourceID);
+                Uri serviceRoot = new Uri(servicePointUri, tenantID);
+                ActiveDirectoryClient activeDirectoryClient = new ActiveDirectoryClient(serviceRoot,
+                      async () => await GetTokenForApplication());
+
+                // use the token for querying the graph to get the user details
+                IUser user = activeDirectoryClient.Users
+                    .Where(u => u.ObjectId.Equals(userObjectID))
+                    .ExecuteAsync().Result.CurrentPage.ToList().First();
+
+                return View(user);
+            }
+            // if the above failed, the user needs to explicitly re-authenticate for the app to obtain the required token
+            catch (AdalException)
+            {
+                ViewBag.ErrorMessage = "Error creating ADAL";
+                return View();
+            }
+            // if the above failed, the user needs to explicitly re-authenticate for the app to obtain the required token
+            catch (Exception)
+            {
+                ViewBag.ErrorMessage = "AuthorizationRequired";
+                return View();
+            }
+        }
+
+        public void RefreshSession()
+        {
+            HttpContext.GetOwinContext().Authentication.Challenge(
+                new AuthenticationProperties { RedirectUri = "/UserProfile" }, 
+                OpenIdConnectAuthenticationDefaults.AuthenticationType);
+        }
+
+        public async Task<string> GetTokenForApplication()
+        {
+            string signedInUserID = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+            string tenantID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+            string userObjectID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+
+            // get a token for the Graph without triggering any user interaction (from the cache, via multi-resource refresh token, etc)
+            ClientCredential clientcred = new ClientCredential(clientId, appKey);
+            // initialize AuthenticationContext with the token cache of the currently signed in user, as kept in the app's database
+            AuthenticationContext authenticationContext = new AuthenticationContext(string.Format("https://login.windows.net/{0}", tenantID), new ADALTokenCache(signedInUserID));
+            AuthenticationResult authenticationResult = await authenticationContext.AcquireTokenSilentAsync(graphResourceID, clientcred, new UserIdentifier(userObjectID, UserIdentifierType.UniqueId));
+            return authenticationResult.AccessToken; 
+        }
+    }
+}
